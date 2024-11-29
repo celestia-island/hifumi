39c31077a5fad166dac2e086de8bd232c281fdf1
伊欧 <langyo.china@gmail.com>
2024-11-30 02:00:01 +0800

    🧪 Add unit tests for timestamp.

--- packages/types/tests/basic.rs
+++ packages/types/tests/basic.rs
@@ -2,7 +2,18 @@
 mod test {
     #[test]
     fn decl_single_version() {
-        #[version("0.1")]
+        #[version(0.1)]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+    }
+
+    #[test]
+    fn decl_single_version_with_suffix() {
+        #[version(1.0.0-rc12)]
         #[derive(Debug, Clone, PartialEq)]
         struct Test {
             a: i32,
@@ -13,7 +24,7 @@ mod test {
 
     #[test]
     fn decl_old_version() {
-        #[version("0.2")]
+        #[version(0.2)]
         #[derive(Debug, Clone, PartialEq)]
         struct Test {
             a: i32,
@@ -23,7 +34,7 @@ mod test {
 
         #[old_version]
         {
-            #[version("0.1")]
+            #[version(0.1)]
             #[derive(Debug, Clone, PartialEq)]
             struct {
                 a: i32,
@@ -31,13 +42,13 @@ mod test {
                 c: i32,
             }
 
-            migration!("0.1" => "0.2");
+            migration!(0.1 => 0.2);
         }
     }
 
     #[test]
     fn decl_multiple_old_versions() {
-        #[version("0.3")]
+        #[version(0.3)]
         #[derive(Debug, Clone, PartialEq)]
         struct Test {
             a: i32,
@@ -47,7 +58,7 @@ mod test {
 
         #[old_version]
         {
-            #[version("0.2")]
+            #[version(0.2)]
             #[derive(Debug, Clone, PartialEq)]
             struct {
                 a: i32,
@@ -55,16 +66,59 @@ mod test {
                 c: i32,
             }
 
-            migration!("0.2" => "0.3");
+            migration!(0.2 => 0.3);
 
-            #[version("0.1")]
+            #[version(0.1)]
             #[derive(Debug, Clone, PartialEq)]
             struct {
                 a: i32,
                 b: i32,
             }
 
-            migration!("0.1" => "0.2");
+            migration!(0.1 => 0.2);
+        }
+    }
+
+    #[test]
+    fn decl_version_with_timestamp() {
+        #[version(2025-1-1)]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        #[version(2025-01-01)]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test2 {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        #[version(2025-1-1 9:34)]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test3 {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        #[version(2025-1-01 09:34:12)]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test4 {
+            a: i32,
+            b: i32,
+            c: i32,
+        }
+
+        #[version(unix(12345678))]
+        #[derive(Debug, Clone, PartialEq)]
+        struct Test5 {
+            a: i32,
+            b: i32,
+            c: i32,
         }
     }
 }
