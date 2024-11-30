4532eb10279920a217c5d149abd0aa9f489c4c20
伊欧 <langyo.china@gmail.com>
2024-12-01 03:37:55 +0800

    🔊 Add some notes.

---
+++ packages/macros/src/template/schema_builder.rs
@@ -0,0 +1,2 @@
+// TODO: 对于 JSON schema，version 宏的参数即为 $id 的值，并且每个版本均允许独立生成
+//       对于 XML schema，version 宏的参数即为 targetNamespace 的值，并且每个版本均允许独立生成
--- packages/macros/src/tools/migration.rs
+++ packages/macros/src/tools/migration.rs
@@ -1,7 +1,36 @@
-use syn::parse::{Parse, ParseStream};
+use syn::{
+    parse::{Parse, ParseStream},
+    Path,
+};
 
 #[derive(Debug, Clone)]
-pub struct Migration {}
+pub struct Migration {
+    pub from: String,
+    pub to: String,
+    pub changes: Vec<Change>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Change {
+    AddField {
+        key: String,
+        ty: Path,
+    },
+    RemoveField {
+        key: String,
+        ty: Path,
+    },
+    RenameField {
+        source: Vec<(String, Path)>,
+        target: String,
+        ty: Path,
+    },
+    CopyField {
+        source: Vec<(String, Path)>,
+        target: String,
+        ty: Path,
+    },
+}
 
 impl Parse for Migration {
     fn parse(input: ParseStream) -> syn::Result<Self> {
