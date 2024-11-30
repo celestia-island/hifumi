9aa9c026bf7f107275685ad80174297371844347
伊欧 <langyo.china@gmail.com>
2024-11-30 17:08:28 +0800

    🧱 Create macros.

---
+++ packages/macros/src/template/mod.rs
@@ -0,0 +1,7 @@
+pub(crate) mod current_version_struct;
+pub(crate) mod impl_migration;
+pub(crate) mod old_version_structs;
+
+pub(crate) use current_version_struct::generate_current_version_struct;
+pub(crate) use impl_migration::generate_impl_migration;
+pub(crate) use old_version_structs::generate_old_version_structs;
