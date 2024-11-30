c138c2f295beda17550266f04669e921d19ef85c
伊欧 <langyo.china@gmail.com>
2024-11-30 22:08:25 +0800

    📝 Parse version macro attribute.

--- packages/macros/src/tools/derive_version.rs
+++ packages/macros/src/tools/derive_version.rs
@@ -1,10 +1,23 @@
-use syn::parse::{Parse, ParseStream};
+use syn::{
+    parse::{Parse, ParseStream},
+    Expr,
+};
 
 #[derive(Debug, Clone)]
-pub struct DeriveVersion {}
+pub struct DeriveVersion {
+    pub version: String,
+}
 
 impl Parse for DeriveVersion {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            version: match input.parse::<Expr>()? {
+                Expr::Lit(lit) => match lit.lit {
+                    syn::Lit::Str(s) => s.value(),
+                    _ => return Err(syn::Error::new(input.span(), "Expected a string literal")),
+                },
+                _ => return Err(syn::Error::new(input.span(), "Expected a string literal")),
+            },
+        })
     }
 }
