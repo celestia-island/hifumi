c093ec985fad84be5f16da89caa366d892ae31fb
伊欧 <langyo.china@gmail.com>
2024-12-02 05:25:31 +0800

    🤡 Write some expanded code for reference.

--- packages/macros/src/template/mod.rs
+++ packages/macros/src/template/mod.rs
@@ -1,6 +1,7 @@
 pub(crate) mod current_version_struct;
 pub(crate) mod impl_migration;
 pub(crate) mod old_version_structs;
+pub(crate) mod schema_builder;
 
 pub(crate) use current_version_struct::generate_current_version_struct;
 pub(crate) use impl_migration::generate_impl_migration;
