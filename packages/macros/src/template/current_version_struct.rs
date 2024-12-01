a4efe6c7aec308585ab27f0c2372b0808729bc96
伊欧 <langyo.china@gmail.com>
2024-12-02 05:25:31 +0800

    🤡 Write some expanded code for reference.

--- packages/macros/src/template/current_version_struct.rs
+++ packages/macros/src/template/current_version_struct.rs
@@ -1,5 +1,15 @@
+use anyhow::Result;
 use proc_macro2::TokenStream;
+use quote::quote;
+use syn::Ident;
 
-pub(crate) fn generate_current_version_struct() -> Vec<TokenStream> {
-    todo!()
+use crate::tools::DeriveVersion;
+
+pub(crate) fn generate_current_version_struct(
+    name: Ident,
+    input: DeriveVersion,
+) -> Result<TokenStream> {
+    dbg!(name, input);
+
+    Ok(quote! {})
 }
