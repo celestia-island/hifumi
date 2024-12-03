a949d1fa93f6c4d70f55c5f69ae005c261eca370
伊欧 <langyo.china@gmail.com>
2024-12-04 03:34:58 +0800

    ♻️ Move migration parser into attr macros.

--- packages/macros/src/tools/mod.rs
+++ packages/macros/src/tools/mod.rs
@@ -1,5 +1,9 @@
 pub(crate) mod derive_version;
 pub(crate) mod migration;
+pub(crate) mod migration_comment;
+pub(crate) mod migration_field;
 
 pub(crate) use derive_version::DeriveVersion;
 pub(crate) use migration::Migration;
+pub(crate) use migration_comment::MigrationComment;
+pub(crate) use migration_field::MigrationField;
