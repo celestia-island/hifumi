887aa3435c5beed65c446d10d1fb64942494a0de
伊欧 <langyo.china@gmail.com>
2024-12-05 14:43:47 +0800

    📝 Add old version structs' generator.

--- packages/macros/src/template/old_version_structs.rs
+++ packages/macros/src/template/old_version_structs.rs
@@ -1,5 +1,86 @@
+use anyhow::Result;
 use proc_macro2::TokenStream;
+use quote::quote;
+use std::collections::HashMap;
+use syn::{Ident, Type};
 
-pub(crate) fn generate_old_version_structs() -> Vec<TokenStream> {
-    todo!()
+use crate::{tools::MigrationField, utils::generate_ident};
+
+// Do the opposite operation and roll back to the old version
+fn generate_older_version_struct(
+    old_struct_fields: HashMap<Ident, Type>,
+    convert_rules: Vec<MigrationField>,
+) -> Result<HashMap<Ident, Type>> {
+    let mut struct_fields = old_struct_fields.to_owned();
+
+    for rule in convert_rules.iter() {
+        match rule {
+            MigrationField::AddField { value, .. } => {
+                let (key, _) = value;
+                struct_fields.remove(&key);
+            }
+            MigrationField::CopyField { target, .. } => {
+                let (target_ident, _) = target;
+                struct_fields.remove(&target_ident);
+            }
+            MigrationField::RemoveField { value } => {
+                let (ident, ty) = value;
+                struct_fields.insert(ident.clone(), Type::Path(ty.clone()));
+            }
+            MigrationField::RenameField { source, target, .. } => {
+                for (ident, ty) in source.iter() {
+                    struct_fields.insert(ident.clone(), Type::Path(ty.clone()));
+                }
+
+                let (target_ident, _) = target;
+                struct_fields.remove(&target_ident);
+            }
+        }
+    }
+
+    Ok(struct_fields.into_iter().collect())
+}
+
+pub(crate) fn generate_old_version_structs(
+    ident: Ident,
+    final_version: String,
+    final_struct_fields: HashMap<Ident, Type>,
+    extra_macros: Vec<TokenStream>,
+    versions: Vec<(String, Vec<MigrationField>)>,
+) -> Result<TokenStream> {
+    let mut temp_struct_fields = final_struct_fields.to_owned();
+    let mut old_version_structs = vec![(final_version, temp_struct_fields.clone())];
+
+    for (version, convert_rules) in versions.iter() {
+        temp_struct_fields =
+            generate_older_version_struct(temp_struct_fields.clone(), convert_rules.to_owned())?;
+        old_version_structs.push((version.clone(), temp_struct_fields.clone()));
+    }
+
+    let old_version_structs = old_version_structs
+        .iter()
+        .map(|(version, fields)| {
+            let struct_name = generate_ident(&ident, version)?;
+            let struct_name = Ident::new(&struct_name, ident.span());
+            let fields = fields.iter().map(|(ident, ty)| {
+                quote! {
+                    #ident: #ty,
+                }
+            });
+
+            Ok(quote! {
+                #[doc(hidden)]
+                #(#extra_macros)*
+                pub struct #struct_name {
+                    #(#fields)*
+                }
+            })
+        })
+        .collect::<Vec<Result<TokenStream>>>()
+        .into_iter()
+        .collect::<Result<Vec<TokenStream>>>()?;
+
+    Ok(quote! {
+        #(#old_version_structs)*
+    })
 }
